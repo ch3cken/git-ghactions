8e3209839e9cd547e29a24ccf45ec2b2b6f9658b
ChEcKeN <sonerplayer8@gmail.com>
2026-04-08 13:47:21 +0900

    test case correction (2)

--- test_triangle.py
+++ test_triangle.py
@@ -2,4 +2,4 @@ from triangle import triangle
 def test_invalid1():
     assert triangle(-1, 0, 1) == -1
 def test_equilateral():
-    assert triangle(3, 3, 3) == 1
+    assert triangle(3, 3, 3) == 2
